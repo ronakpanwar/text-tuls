f116f8bc7e27caeaf8f2261bdd3e0417ee9f3ecc
ronakpanwar <ronakpanwar18@gmail.com>
2023-10-11 19:01:06 +0530

    thired

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -6,7 +6,7 @@ function Navbar(props) {
 <>
 <nav class={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}` } >
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">translater</a>
+    <a class="navbar-brand" href="#">text-tuls</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
